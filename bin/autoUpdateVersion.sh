b4c1e471dea5b4012dc7ca6bcd252a28b450ed1c
xiangdong <xiang.d@vip.163.com>
2015-11-28 15:54:09 +0800

    version-0.2.0

--- bin/autoUpdateVersion.sh
+++ bin/autoUpdateVersion.sh
@@ -2,6 +2,11 @@
 
 #调用方式
 # autoUpdateVersion.sh [revision]  revision 可选，默认为 SNAPSHOT
+# xiangdong 2015.11.20
+
+#找到该脚本的绝对路径，防止不到此目录执行脚本，导致相对路径出错
+basepath=$(cd `dirname $0`; pwd)
+cd $basepath
 
 #从版本配置文件获取版本相关信息到环境变量
 source ../version.properties
@@ -12,7 +17,7 @@ if [ -n "$1" ]; then
 fi
 
 #形成完整的版本信息
-version=$major.$minor.$build-$revision
+version=$major.$minor.$release.$regression-$revision
 
 echo newversion:$version
 
@@ -47,19 +52,18 @@ sedArgs="s/<version><!--parent-->.*$/<version><!--parent-->${version}<\/version>
 sed -i ""  $sedArgs ../uc-web/pom.xml
 echo "uc-web pom.xml update parent version to ${version} OK."
 
-
-#不是快照版本需要更新build
+#不是快照版本需要更新regression
 if [ "$revision" != "SNAPSHOT" ]; then
-	#更新$build, 自动 + 1
-	let build=$build+1
+	#更新$regression, 自动 + 1
+	let regression=$regression+1
 
 	#更新版本配置文件
-	sedArgs="s/build.*$/build=${build}/"
+	sedArgs="s/regression=.*$/regression=${regression}/"
 	sed -i ""  $sedArgs ../version.properties
 	echo "version.properties update OK."
 
 	#更新提交到版本库
-	#git add ../version.properties
-	#git commit -m "release v${version}"
-	#echo "git commit OK."
+	git add ../version.properties
+	git commit -m "regression ${version}"
+	echo "git commit OK."
 fi
