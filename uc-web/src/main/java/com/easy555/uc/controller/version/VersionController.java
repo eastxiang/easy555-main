9b62dff6779dc5bbb8d23d8dc6f7ea190489344b
xiangdong <xiang.d@vip.163.com>
2015-12-05 22:03:23 +0800

    version info set.

--- uc-web/src/main/java/com/easy555/uc/controller/version/VersionController.java
+++ uc-web/src/main/java/com/easy555/uc/controller/version/VersionController.java
@@ -1,11 +1,38 @@
 package com.easy555.uc.controller.version;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 显示工程的版本号<br>
+ * 版本号由maven自动替换形成.
+ * 
+ * create date: 2015年12月5日 下午9:59:04
+ * 
+ * @author xiangdong
+ */
+
+@Controller
 public class VersionController {
 
-	// POM.xml 配置自动替换成当前版本号;
-	@Value(value = "${system.current.version}")
-	final String version = "0.0.1";
-	
+	/**
+	 * POM.xml 配置自动替换成当前版本号;
+	 */
+	@Value("${system.current.version}")
+	private String currentVersion = "0.1";
+
+	/**
+	 * 获取当期版本号
+	 * 
+	 * @return 版本号
+	 */
+	@ResponseBody
+	@RequestMapping("/version")
+	public String version(HttpServletRequest request) {
+		return request.getContextPath() + ": " + currentVersion;
+	}
 }
--- uc-web/src/main/java/org/apache/shiro/realm/UserRealm.java
+++ uc-web/src/main/java/org/apache/shiro/realm/UserRealm.java
@@ -141,5 +141,4 @@ public class UserRealm extends AuthorizingRealm {
 				getName());
 		return info;
 	}
-
 }
